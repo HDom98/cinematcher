80dfd85eaa5fecbc57968acccb76bee014ecb858
HDom98 <55728875+HDom98@users.noreply.github.com>
2021-12-05 10:06:16 -0700

    added TabBar to navigate to different screens

--- main.dart
+++ main.dart
@@ -1,44 +1,46 @@
+import 'package:cinematcher/route_generator.dart';
+import 'package:cinematcher/screens/genre_picker.dart';
+import 'package:cinematcher/screens/watchlist.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Cinematcher());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Cinematcher extends StatelessWidget {
+  const Cinematcher({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cinematcher',
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.generateRoute,
       theme: ThemeData(
         primarySwatch: Colors.red,
         canvasColor: Colors.red
       ),
-      home: const Center(child: MyHomePage(title: 'Cinematcher')),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title)
+      home: const DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          bottomNavigationBar: TabBar(
+            tabs: [Tab(
+              text: "Recommend a movie",
+              icon: Icon(Icons.movie),
+            ),
+              Tab(
+                text: "Watchlist",
+                icon: Icon(Icons.list),
+              ),],
+          ),
+          body: TabBarView(
+            children: [
+              GenrePicker(),
+              Watchlist()
+            ],
+          ),
+        ),
       ),
     );
-
   }
 }
